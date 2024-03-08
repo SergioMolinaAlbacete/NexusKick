95296a8b5231b024e82b732fc198f761552e0c19
Sergio Molina Albacete <sergio11albacete@gmail.com>
2024-03-08 13:12:53 +0100

    cambios

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import './App.css';
 
-const App = () => <div>My Whishlist app</div>;
+const wishes = [
+    { text: 'Travel tpo the moon', done: false },
+    { text: 'Pay the gym', done: true },
+    { text: 'Go to the gym', done: false }
+]
+
+const App = () =>
+    <div>
+        <h1>My Whishlist app</h1>
+        <fieldset>
+            <legend>New wish</legend>
+            <input placeholder="Enter your wish" />
+        </fieldset>
+        <ul>
+            {wishes.map(({ text, done }, i) => (
+                <li>
+                    <label htmlFor={`wish${i}`}>
+                        <input id={`wish${i}`} type="checkbox" checked={done} />
+                        {text}
+                    </label>
+                </li>
+            ))}
+        </ul>
+        <button type="button">Archive done</button>
+    </div>;
 
 export default App;
