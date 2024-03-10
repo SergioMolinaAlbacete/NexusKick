1f234370a595b11417bc1ba11c570bcc1111ab86
Sergio Molina Albacete <81566137+WhiteCleaverPC@users.noreply.github.com>
2024-03-10 11:21:44 +0100

    ejercicio1terminado

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,11 +5,18 @@ import WishInput from "./WishInput";
 import WishList from "./WishList";
 
 
+const wishes = [
+    { text: 'Travel tpo the moon', done: false },
+    { text: 'Pay the gym', done: true },
+    { text: 'Go to the gym', done: false }
+]
+
+
 const App = () =>
     <div className="app">
         <h1>My Whishlist app</h1>
         <WishInput />
-        <WishList />
+        <WishList wishes={wishes}/>
         <button className="wish-clear" type="button">Archive done</button>
     </div>;
 
--- src/App/WishList/WishList.jsx
+++ src/App/WishList/WishList.jsx
@@ -1,23 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const wishes = [
-    { text: 'Travel tpo the moon', done: false },
-    { text: 'Pay the gym', done: true },
-    { text: 'Go to the gym', done: false }
-]
-
-const WishList = ({wishes}) => (
+const WishList = ({ wishes }) => (
     <ul className="wish-list">
-            {wishes.map(({ text, done }, i) => (
-                <li key={text} className={`wish-list__item ${done ? 'wish-list__item--done' : ''} `}> 
-                                                                                                {/* Eso podria hacerse utilizando la libreria classNames */}
-                    <label htmlFor={`wish${i}`}>
-                        <input id={`wish${i}`} type="checkbox" checked={done} />
-                        {text}
-                    </label>
-                </li>
-            ))}
-        </ul>
+        {wishes.map(({ text, done }, i) => (
+            <li key={text} className={`wish-list__item ${done ? 'wish-list__item--done' : ''} `}>
+                {/* Eso podria hacerse utilizando la libreria classNames */}
+                <label htmlFor={`wish${i}`}>
+                    <input id={`wish${i}`} type="checkbox" checked={done} />
+                    {text}
+                </label>
+            </li>
+        ))}
+    </ul>
 );
+
+WishList.PropTypes = {
+    wishes: PropTypes.arrayOf(
+        PropTypes.shape({
+            done: PropTypes.bool,
+            text: PropTypes.string,
+        })),
+};
+
+WishList.defaultProps = {
+    wishes: [],
+}
 
 export default WishList;
