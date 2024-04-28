33e322405a5d90d7201fb6002876f2dd4fdb1e8c
Sergio Molina Albacete <sergio11albacete@gmail.com>
2024-04-28 10:47:42 +0200

    cambios

--- nexuskick/src/App.js
+++ nexuskick/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Components/Navbar.jsx';
+import './App.css'; // Asegúrate de que exista y esté en la misma carpeta que App.jsx
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          {/* Aquí van tus rutas, por ejemplo: */}
+          <Route path="/jugadores">
+            {/* Componente de Jugadores */}
+          </Route>
+          {/* Asegúrate de agregar las rutas para entrenadores y equipos */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
