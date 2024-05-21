17027aeb28dd26c3141cc8d3eeaad384aebae4e2
Sergio Molina Albacete <sergio11albacete@gmail.com>
2024-05-21 09:33:35 +0200

    MEJORAS

--- NexusKickPHP/src/php/views/verPerfilEntrenador.php
+++ NexusKickPHP/src/php/views/verPerfilEntrenador.php
@@ -1,1 +1,190 @@
-<!-- hacer -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Perfil | NexusKick</title>
+    <link rel="stylesheet" href="./../../css/general.css">
+    <link rel="stylesheet" href="./../../css/header.css">
+    <link rel="stylesheet" href="./../../css/perfil.css">
+
+</head>
+
+<body>
+    <?php
+    include './componentes/header.php';
+    include '../config/db.php'; // Asegúrate de que la ruta es correcta
+
+    //Comprobar si el usuario ha iniciado sesión
+    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+        header('Location: login.php');
+        exit;
+    }
+
+    $usuario_id = $_SESSION['id_usuario'];  // Aquí accedes al ID del usuario desde la sesión
+
+
+    // Consulta para obtener la información del jugador
+    $sql = "SELECT * FROM usuarios WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $usuario_id);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    $entrenador = $resultado->fetch_assoc();
+    ?>
+
+
+
+    <div class="titulo-container">
+        <h1>Perfil de <?= $entrenador['nombre'] ?> <?= $entrenador['apellidos'] ?></h1>
+    </div>
+    <!-- Bloque información personal -->
+    <div class="player-profile">
+        <div class="player-profile-column">
+            <img src="<?= !empty($entrenador['perfil_url']) ? $entrenador['perfil_url'] : '../../img/imagenPerfilPredeterminada.jpg' ?>" alt="<?= htmlspecialchars($entrenador['nombre']) ?>" class="player-image" />
+            <h2 class="player-nickname"><?= $entrenador['apodo'] ?></h2>
+        </div>
+        <div class="player-info">
+            <h2>Información Personal</h2>
+            <p><span class="info-label">Nombre Completo:</span> <?= $entrenador['nombre'] ?> <?= $entrenador['apellidos'] ?></p>
+            <p><span class="info-label">País de Nacimiento:</span> <?= $entrenador['pais'] ?></p>
+            <p><span class="info-label">Lugar de Nacimiento:</span> <?= $entrenador['ciudad'] ?></p>
+            <p><span class="info-label">Fecha de Nacimiento:</span> <?= $entrenador['fnacimiento'] ?></p>
+            <p><span class="info-label">Edad:</span> <?= $entrenador['edad'] ?> años</p>
+            <p><span class="info-label">Experiencia:</span> <?= $entrenador['experiencia'] ?> años</p>
+            <p><span class="info-label">Especialidad:</span> <?= $entrenador['especialidad'] ?> años</p>
+            <p><span class="info-label">Talla de Ropa:</span> <?= $entrenador['tallaRopa'] ?></p>
+            <p><span class="info-label">Talla de Calzado:</span> <?= $entrenador['tallaCalzado'] ?></p>
+
+        </div>
+    </div>
+
+
+
+
+
+    <?php
+    // Consulta para obtener el historial deportivo del usuario
+    $sqlHistorial = "SELECT * FROM historial_deportivo WHERE usuario_id = ?";
+    $stmtHistorial = $conn->prepare($sqlHistorial);
+    $stmtHistorial->bind_param("i", $usuario_id);
+    $stmtHistorial->execute();
+    $resultadoHistorial = $stmtHistorial->get_result();
+    $historiales = [];
+    while ($fila = $resultadoHistorial->fetch_assoc()) {
+        $historiales[] = $fila;
+    }
+
+    ?>
+
+    <!-- Bloque Historial -->
+    <div class="player-history">
+        <h2>Historial del jugador</h2>
+        <div class="history-slider">
+            <button class="slide-arrow left-arrow">
+                <img src="../../img/flechaIzquierda.png" alt="Anterior" />
+            </button>
+
+            <?php foreach ($historiales as $item) : ?>
+                <div class="history-card">
+                    <img src="<?= htmlspecialchars($item['imagenEscudo']) ?>" alt="Escudo del equipo <?= htmlspecialchars($item['equipo']) ?>" class="team-shield" />
+                    <h3><?= htmlspecialchars($item['equipo']) ?></h3>
+                    <p>Temporada: <?= htmlspecialchars($item['temporada']) ?></p>
+                    <p>Resultado: <?= htmlspecialchars($item['resultado']) ?></p>
+                </div>
+            <?php endforeach; ?>
+
+            <button class="slide-arrow right-arrow">
+                <img src="../../img/flechaDerecha.png" alt="Siguiente" />
+            </button>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+    // Consulta para obtener la situación académica o laboral del usuario
+    $sqlSituacion = "SELECT id,actividad, lugar, horario FROM situacion_academica_laboral WHERE usuario_id = ?";
+    $stmtSituacion = $conn->prepare($sqlSituacion);
+    $stmtSituacion->bind_param("i", $usuario_id); // Asegúrate de que $usuario_id está definido y corresponde al usuario en sesión
+    $stmtSituacion->execute();
+    $resultadoSituacion = $stmtSituacion->get_result();
+    $situaciones = [];
+    while ($fila = $resultadoSituacion->fetch_assoc()) {
+        $situaciones[] = $fila;
+    }
+    ?>
+
+    <!-- Bloque Situacion -->
+    <div class="work-reviews-container">
+        <div class="work-info">
+            <h2>Situación Laboral y/o Estudio</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Actividad</th>
+                        <th>Lugar</th>
+                        <th>Horario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($situaciones as $situacion) : ?>
+                        <tr>
+                            <td><?= htmlspecialchars($situacion['actividad']) ?></td>
+                            <td><?= htmlspecialchars($situacion['lugar']) ?></td>
+                            <td><?= htmlspecialchars($situacion['horario']) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+
+
+
+        <?php
+        // Consulta para obtener las reseñas y las fotos de perfil de los usuarios que las escribieron
+        $sqlReseñas = "SELECT *
+                   FROM reseñas r
+                   JOIN usuarios u ON r.de_usuario_id = u.id
+                   WHERE r.para_usuario_id = ?";
+        $stmtReseñas = $conn->prepare($sqlReseñas);
+        $stmtReseñas->bind_param("i", $usuario_id);
+        $stmtReseñas->execute();
+        $resultadoReseñas = $stmtReseñas->get_result();
+        $reseñas = [];
+        while ($fila = $resultadoReseñas->fetch_assoc()) {
+            $reseñas[] = $fila;
+        }
+        ?>
+
+
+        <!-- Bloque Reseñas -->
+        <div class="reviews">
+            <h2>Reseñas<button id="añadirReseña">Añadir reseña</button></h2>
+            <?php foreach ($reseñas as $reseña) : ?>
+                <div class="review">
+                    <!-- Mostrar la foto de perfil si está disponible, de lo contrario mostrar una imagen por defecto -->
+                    <img src="<?= $reseña['perfil_url'] ?: '../img/imagenPerfilPredeterminada.jpg' ?>" alt="Foto de perfil" style="width: 50px; height: 50px; border-radius: 50%;">
+                    <p><?= htmlspecialchars($reseña['reseña']) ?></p>
+                    <p><?= str_repeat("⭐", intval($reseña['puntuacion'])) ?></p>
+                    <small><?= date("d/m/Y", strtotime($reseña['fecha'])) ?></small>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+
+
+ 
+
+</body>
+
+</html>
